05c864b49f7ebfbc836267835238568cb7abf237
Lilit <tamrazyan.lilit.95@gmail.com>
2021-10-07 16:10:25 +0400

    first commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,131 @@
+<template>
+  <div class="bg-gray">
+    <header class="header wrapper">
+      <nav class="flex-center-between">
+        <div>
+          <img src="../assets/logo1.svg" alt="logo" class="header-logo" />
+        </div>
+        <ul class="menu flex menu-md-none">
+          <li class="menu__item">
+            <router-link to="/downloads">Downloads</router-link>
+          </li>
+          <li class="menu__item">
+            <router-link to="/ecosystem">Ecosystem</router-link>
+          </li>
+          <li class="menu__item">
+            <router-link to="/team">The Team</router-link>
+          </li>
+          <li class="menu__item">
+            <router-link to="/investors">Investors</router-link>
+          </li>
+        </ul>
+        <div class="menu-icon" @click="showMobileMenu = !showMobileMenu">
+          <img src="../assets/menu.png" alt="menu-icon" width="40" />
+        </div>
+        <transition name="fade">
+          <ul class="menu flex" v-if="showMobileMenu" :class="showMobileMenu ? 'show-mobile-menu' : ''">
+            <li class="menu__item" @click="showMobileMenu = !showMobileMenu">
+              <router-link to="/downloads">Downloads</router-link>
+            </li>
+            <li class="menu__item">
+              <router-link to="/ecosystem">Ecosystem</router-link>
+            </li>
+            <li class="menu__item">
+              <router-link to="/team">The Team</router-link>
+            </li>
+            <li class="menu__item">
+              <router-link to="/investors">Investors</router-link>
+            </li>
+          </ul>
+        </transition>
+      </nav>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  data() {
+    return {
+      showMobileMenu: false,
+    }
+  }
+}
+</script>
+
+<style scoped>
+.fade-enter-active, .fade-leave-active {
+  transition-property: opacity;
+  transition-duration: .25s;
+}
+
+.fade-enter-active {
+  transition-delay: .25s;
+}
+
+.fade-enter, .fade-leave-active {
+  opacity: 0
+}
+.header {
+  position: relative;
+  padding-top: 25px;
+  padding-bottom: 25px;
+}
+.show-mobile-menu {
+  position: absolute;
+  width: 100%;
+  background-color: bisque;
+  display: block;
+  /*height: calc(35vh - 106px);*/
+  top: 95px;
+  left: 0;
+  padding: 10px 0;
+  transition: 2s all ease;
+}
+.show-mobile-menu .menu__item {
+  padding: 10px 20px;
+}
+.menu__item {
+  padding: 0 24px;
+  font-weight: 600;
+  font-size: 20px;
+  line-height: 24px;
+}
+.menu__item a {
+  color: #323232;
+  text-decoration: none;
+}
+.menu-icon {
+  display: none;
+}
+.header .router-link-exact-active.router-link-active {
+  font-weight: 800;
+  color: #8F63D6;
+}
+@media (max-width: 1600px) {
+  .header-logo {
+    max-width: 140px;
+  }
+}
+@media (max-width: 992px) {
+  .menu-icon {
+    display: block;
+  }
+  .menu-md-none {
+    display: none;
+  }
+}
+@media (max-width: 600px) {
+  .show-mobile-menu {
+    top: 75px;
+  }
+  .header {
+    padding-top: 15px;
+    padding-bottom: 15px;
+  }
+  .header-logo {
+    max-width: 110px;
+  }
+}
+</style>
---
+++ src/views/Team.vue
@@ -0,0 +1,385 @@
+<template>
+  <div class="team">
+    <div class="team-top-block w-50 text-center">
+      <h1 class="title text-bold text-primary">We Are Zardite</h1>
+      <img src="../assets/divider.svg" alt="divider" />
+      <p class="team-info text-bold">
+        The passionate few with a flare for gaming in their hearts and
+        a vision to connect as many people as possible through social gaming.
+      </p>
+    </div>
+    <div class="team-member flex-start-center flex-sm-column mb-115">
+      <div class="team-member__block">
+        <img src="../assets/images/team/team-member1.svg" alt="team-member" />
+        <p class="team-member__name">Eugene Joseph</p>
+        <span class="team-member__profession">CEO/Co-Founder</span>
+      </div>
+      <div class="team-member__block">
+        <img src="../assets/images/team/team-member2.svg" alt="team-member" />
+        <p class="team-member__name">Dimitri Miles</p>
+        <span class="team-member__profession">CEO/Co-Founder</span>
+      </div>
+      <div class="team-member__block">
+        <img src="../assets/images/team/team-member3.svg" alt="team-member" />
+        <p class="team-member__name">Tamara Korcak</p>
+        <span class="team-member__profession">CSO/Cheif Strategic Officer</span>
+      </div>
+    </div>
+    <div class="team-member flex-start-center flex-sm-column mb-124">
+      <div class="team-member__block">
+        <img src="../assets/images/team/team-member4.svg" alt="team-member" />
+        <p class="team-member__name">Lasha Abazashvili</p>
+        <span class="team-member__profession">Back-End Developer</span>
+      </div>
+      <div class="team-member__block">
+        <img src="../assets/images/team/team-member5.svg" alt="team-member" />
+        <p class="team-member__name">Parin Thakrar</p>
+        <span class="team-member__profession">Mobile Application Developer</span>
+      </div>
+      <div class="team-member__block">
+        <img src="../assets/images/team/team-member6.svg" alt="team-member" />
+        <p class="team-member__name">Opeyemi Agbeja</p>
+        <span class="team-member__profession">Mobile Engineer</span>
+      </div>
+      <div class="team-member__block">
+        <img src="../assets/images/team/team-member7.svg" alt="team-member" width="275" />
+        <p class="team-member__name">Shubham Chouhan</p>
+        <span class="team-member__profession">Visual/Communication Designer</span>
+      </div>
+    </div>
+    <div>
+      <p class="us-at-glance text-center text-bold text-italic">Us at a glance</p>
+      <img src="../assets/large-divider.svg" alt="divider" width="100%" />
+    </div>
+    <div class="about-team flex-start-between pt-124 flex-sm-column">
+      <div>
+        <div class="team-member">
+          <div class="team-member__block left">
+            <img src="../assets/images/team/team-member1.svg" alt="team-member" width="100%" />
+            <p class="team-member__name">Eugene Joseph</p>
+            <span class="team-member__profession">CEO/Co-Founder</span>
+            <p class="team-member__info">
+              “Possesses unmatched zeal for the Zardite.”
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="message">
+        <img src="../assets/info-right-bg.png" alt="" width="691" />
+        <p class="message__content">
+          Eugene is the creator and founder of Zardite. Originally from a finance background and applying
+          his skills in playing the market, he decided to transition to a different kind of gaming. Eugene
+          enjoys seeing new places, engaging in new experiences, playing sports from football to squash and cycling.
+        </p>
+      </div>
+    </div>
+    <div class="about-team flex-start-between flex-sm-column">
+      <div class="message">
+        <img src="../assets/info-left-svg.png" alt="" width="691" />
+        <p class="message__content">
+          With a passion for gaming and technology, there was no question Dimitri wouldn't have taken up the
+          opportunity to lead Zardite's technical vision.Previously, he led multidisciplinary teams at high
+          profile technology companies including IBM, Vonage & Aristocrat. Dimitri's hobbies include military LARP,
+          cars, flying and travelling.
+          Apart from this, he is a man who is driven to make his dream a reality.
+        </p>
+      </div>
+      <div class="right-block">
+        <div class="team-member">
+          <div class="team-member__block">
+            <img src="../assets/images/team/team-member2.svg" alt="team-member" width="100%" />
+            <p class="team-member__name">Dimitri Miles</p>
+            <span class="team-member__profession">CTO/Co-Founder</span>
+            <p class="team-member__info">
+              “Man with a vision to lead”
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="about-team flex-start-between flex-sm-column">
+      <div>
+        <div class="team-member">
+          <div class="team-member__block left">
+            <img src="../assets/images/team/team-member3.svg" alt="team-member" width="100%" />
+            <p class="team-member__name">Tamara Korcak</p>
+            <span class="team-member__profession">CSO/Cheif Strategic Officer</span>
+            <p class="team-member__info">
+              “Strategic and resourceful.”
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="message">
+        <img src="../assets/info-right-bg.png" alt="" width="691" />
+        <p class="message__content">
+          Tamara is a seasoned digital transformation professional with over 10 years of experience in
+          strategy design and implementation from data driven environments, developing and implementing
+          cross-functional softwares as a service solutions for organisations, both on buyer and seller side.
+          In her current role, Tamara is enabling enterprise customers through digitally transformative solutions
+          to grow and scale.
+        </p>
+      </div>
+    </div>
+    <div class="about-team flex-start-between flex-sm-column">
+      <div class="message">
+        <img src="../assets/info-left-svg.png" alt="" width="691" />
+        <p class="message__content">
+          Brainstorming ideas and and making calculated design decisions is Shubham’s cup of joe. Shubham is a
+          Digital Graphic Designer with over 6 years of experience in design who has designed successful design
+          solutions for a wide variety of brands and companies. An avid learner who loves to talk about the science
+          and reasoning behind design.With a vision to create beautiful experiences that also appreciate
+          functionality Shubham joined hands with Zardite.
+        </p>
+      </div>
+      <div class="right-block">
+        <div class="team-member">
+          <div class="team-member__block">
+            <img src="../assets/images/team/team-member7.svg" alt="team-member" width="100%" />
+            <p class="team-member__name">Shubham Chouhan</p>
+            <span class="team-member__profession">Visual/Communication Designer</span>
+            <p class="team-member__info">
+              “Gets adrenaline rush while talking about design.”
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="about-team flex-start-between flex-sm-column">
+      <div>
+        <div class="team-member">
+          <div class="team-member__block left">
+            <img src="../assets/images/team/team-member5.svg" alt="team-member" width="100%" />
+            <p class="team-member__name">Parin Thakrar</p>
+            <span class="team-member__profession">Mobile Application Developer</span>
+            <p class="team-member__info">
+              “Likes to finetune digital spaces”
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="message">
+        <img src="../assets/info-right-bg.png" alt="" width="691" />
+        <p class="message__content">
+          Mobile Application Developer (React-Native, Android, iOS)Parin started out as a Web Developer,
+          then realized that he’s way more passionate about mobile development. He plays video games , loads of
+          them; especially PC games, and most of his free time he spends on gaming.
+        </p>
+      </div>
+    </div>
+    <div class="about-team flex-start-between flex-sm-column">
+      <div class="message">
+        <img src="../assets/info-left-svg.png" alt="" width="691" />
+        <p class="message__content">
+          Mobile Engineer who’s been developing mobile applications for 4+years now.
+          He started out as a front end web developer before moving to mobile applications .
+          Apart from this, he loves to learn new things. He even plays pian in his free time and
+          loves to hang out with his family whenever he’s not working.
+        </p>
+      </div>
+      <div class="right-block">
+        <div class="team-member">
+          <div class="team-member__block">
+            <img src="../assets/images/team/team-member6.svg" alt="team-member" width="100%" />
+            <p class="team-member__name">Opeyemi Agbeja</p>
+            <span class="team-member__profession">Mobile Engineer</span>
+            <p class="team-member__info">
+              “Plays the role of a wizard in coding.”
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="about-team flex-start-between flex-sm-column">
+      <div>
+        <div class="team-member">
+          <div class="team-member__block left">
+            <img src="../assets/images/team/team-member4.svg" alt="team-member" width="100%" />
+            <p class="team-member__name">Lasha Abazashvili</p>
+            <span class="team-member__profession">Back-End Developer</span>
+            <p class="team-member__info">
+              “A passionate man with a simple ideology”
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="message">
+        <img src="../assets/info-right-bg.png" alt="" width="691" />
+        <p class="message__content">
+          Lasha is Back-end developer who is super passionate about
+          his job and craft - so much so, that even on his free time,
+          he enjoys learning new things.
+          He's also PlayStation and PC gamer, with a fondness for playing to a competitive level, when time permits.
+          With his love for the gaming space and staying ahead of the curve in the software development, Zardite and
+          Lasha have proven to be a perfect match.
+          Outside of the tech space, Lasha enjoys travel and has great interest in ancient architecture.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+name: "Team"
+}
+</script>
+
+<style scoped>
+.team {
+  background: #f3f3f3;
+  padding-top: 85px;
+}
+.team-top-block  {
+  margin: auto;
+  padding-bottom: 116px;
+}
+.team-info {
+  font-size: 30px;
+  line-height: 35px;
+  color: #535353;
+  margin: 20px 18px 0;
+}
+.team-member__block {
+  padding: 0 50px;
+  text-align: center;
+}
+.team-member__name {
+  font-style: normal;
+  font-weight: bold;
+  font-size: 30px;
+  line-height: 47px;
+  color: #7347D1;
+  margin-top: 22px;
+}
+.team-member__profession {
+  font-size: 20px;
+  line-height: 32px;
+}
+.us-at-glance {
+  font-size: 36px;
+  line-height: 42px;
+  color: rgba(43, 43, 43, 0.7);
+  margin-bottom: 12px;
+}
+/* about team */
+.about-team  {
+  flex-wrap: wrap;
+  max-width: 1450px;
+  width: 100%;
+  margin: auto;
+  padding-bottom: 50px;
+}
+.about-team > div {
+  width: 47%;
+}
+.about-team .team-member__block {
+  max-width: 410px;
+  padding: 0;
+}
+.about-team .team-member__block.left {
+  position: relative;
+  padding: 0;
+}
+.about-team .team-member__block img {
+  max-width: 407px;
+}
+.about-team .team-member__name {
+  font-weight: bold;
+  font-size: 40px;
+  line-height: 63px;
+  margin-top: 32px;
+}
+.about-team .team-member__profession {
+  font-size: 24px;
+}
+.about-team .team-member__info {
+  max-width: 413px;
+  width: max-content;
+  font-style: italic;
+  font-weight: normal;
+  font-size: 36px;
+  line-height: 42px;
+  color: rgba(43, 43, 43, 0.7);
+  text-align: start;
+  position: absolute;
+  top: 65px;
+  left: 80%;
+}
+.message {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  position: relative;
+}
+.message__content {
+  position: absolute;
+  padding: 65px 67px;
+  font-weight: 500;
+  font-size: 20px;
+  line-height: 32px;
+  color: #422A69;
+}
+.about-team .right-block {
+  max-width: 410px;
+  position: relative;
+}
+.about-team .right-block .team-member__info {
+  max-width: 410px;
+  position: absolute;
+  top: 17%;
+  left: inherit;
+  right: 59%;
+  padding: 50px;
+}
+
+@media (max-width: 600px) {
+  .team {
+    padding-top: 50px;
+  }
+  .team-info {
+    font-size: 22px;
+  }
+  .team-top-block {
+    padding-bottom: 50px;
+  }
+  .team-member__block {
+    margin-bottom: 25px;
+  }
+  .team-member__name {
+    margin-top: 16px;
+  }
+  .team-member.mb-115 {
+    margin-bottom: 0;
+  }
+  .team-member.mb-124 {
+    padding-bottom: 25px;
+  }
+  .about-team {
+    padding: 0 20px;
+  }
+  .about-team > div {
+    width: 100%;
+  }
+  .message__content {
+    padding: 46px 32px;
+    font-size: 16px;
+  }
+  .about-team .team-member__block img {
+    max-width: 300px;
+  }
+  .about-team .team-member__name {
+    font-size: 30px;
+    margin-top: 16px;
+  }
+  .about-team .team-member__profession {
+    font-size: 20px;
+  }
+  .about-team .team-member__info {
+    max-width: 300px;
+    font-size: 26px;
+    padding-right: 75px;
+    left: 72%;
+  }
+}
+</style>
